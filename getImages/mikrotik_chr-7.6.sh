4a8c5430493f818f133ecfc6d2f6f091942d6210
bayuardhyanto <bayu@celepuk.com>
2024-03-07 16:57:13 +0700

    Update zip Command

--- getImages/mikrotik_chr-7.6.sh
+++ getImages/mikrotik_chr-7.6.sh
@@ -12,7 +12,7 @@ cd /opt/unetlab/addons/qemu/mikrotik-7.14/
 
 wget https://download.mikrotik.com/routeros/7.14/chr-7.14.img.zip
 
-unzip -v chr-7.14.img.zip
+unzip chr-7.14.img.zip
 mv chr-7.14.img chr-7.14.qcow2
 
 /opt/unetlab/wrappers/unl_wrapper -a fixpermissions
